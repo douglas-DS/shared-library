e5c2d8a55323686450160cdc38ef6893997bb876
Douglas Souza <41555946+douglas-DS@users.noreply.github.com>
2020-02-04 04:13:59 -0300

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,15 +2,15 @@
 
 def call(String buildResult, String step) {
     if ( buildResult == "SUCCESS" ) {
-        slackSend color: "good", message: "Job: '${env.JOB_NAME}', Buildnumber: '${env.BUILD_NUMBER}', Stage: '${step}' was successful"
+        slackSend color: "good", channel: "${env.JOB_NAME}", message: "Job: '${env.JOB_NAME}', Buildnumber: '${env.BUILD_NUMBER}', Stage: '${step}' was successful"
     }
     else if( buildResult == "FAILURE" ) {
-        slackSend color: "danger", message: "Job: '${env.JOB_NAME}', Buildnumber: '${env.BUILD_NUMBER}', Stage: '${step}' was failed"
+        slackSend color: "danger", channel: "${env.JOB_NAME}", message: "Job: '${env.JOB_NAME}', Buildnumber: '${env.BUILD_NUMBER}', Stage: '${step}' was failed"
     }
     else if( buildResult == "UNSTABLE" ) {
-        slackSend color: "warning", message: "Job: '${env.JOB_NAME}', Buildnumber: '${env.BUILD_NUMBER}', Stage: '${step}' was unstable"
+        slackSend color: "warning", channel: "${env.JOB_NAME}", message: "Job: '${env.JOB_NAME}', Buildnumber: '${env.BUILD_NUMBER}', Stage: '${step}' was unstable"
     }
     else {
-        slackSend color: "danger", message: "Job: '${env.JOB_NAME}', Buildnumber: '${env.BUILD_NUMBER}', Stage: '${step}' its result was unclear"
+        slackSend color: "danger", channel: "${env.JOB_NAME}", message: "Job: '${env.JOB_NAME}', Buildnumber: '${env.BUILD_NUMBER}', Stage: '${step}' its result was unclear"
     }
 }
